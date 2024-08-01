79c97556173258e1243495e40ab1ecacf56d3046
DmitriyNal <dima7kiper@yandex.ru>
2024-08-01 17:37:15 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,16 @@
+First=int(input('Введите число'))
+Second=int(input('Введите второе чиcло'))
+third=int(input('Введите третье число'))
+print(First)
+print(Second)
+print(third)
+
+
+if First==Second !=third or Second==third!=First or First==third!=Second:
+    print(2)
+elif First != Second != third:
+    print(0)
+elif First == Second == third:
+    print(3)
+
+    
